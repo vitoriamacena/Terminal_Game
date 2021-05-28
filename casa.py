2ff9d4549f5b8b9677b1c7cfd7e2b8f4407e028e
CleitonFurst <furstcleiton@gmail.com>
2021-05-27 21:51:14 -0300

    Commit

---
+++ casa.py
@@ -0,0 +1,38 @@
+ print("1 - escovar os dentes")
+        print("2 - Fazer café da manhã")
+        print("3 - Pedir café da manhã")
+        print("4 - Tomar café da manhã")
+      print("5 - Ir pra escola + 4 ")
+      voltar dormir 
+      + 1h se atrsou e perdeu a prova 
+      ir para casa almoçar  
+      almoÇar fora 
+      R$ gastou dinheiro 
+      pegar  o ônibus
+      ir a pé 
+      fazer tarefa 
+      sair com os amigos 
+      sem dinehros
+      jogar um jogo se sim 2 se não fazer tarefa
+      ir ao curso 
+      ir a pe onibus
+      iniico do curso avaliação 1 hora 
+      sair do curso 
+      tirar duvidas com o professor 
+      jantar em casa 
+      ir comer comer fora 
+        pizza 
+        coxinha
+                passar na farmacia 
+                ou hospital 
+                    acabou 
+      ir para casa a pe
+      ou onibus 
+      tomar banho 
+      ir dormir  
+      
+      
+      
+    
+      
+   
