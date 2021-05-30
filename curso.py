636de65cfacea0794d28e8c2523ba8231dc91142
Vitória Macena <vmartins7@gmail.com>
2021-05-30 15:53:12 -0300

    update

---
+++ curso.py
@@ -0,0 +1,6 @@
+class Curso:
+    def __init__(self):
+        self.__avaliacao = 1
+
+    def sair_curso(self):
+        return ''
---
+++ teste.py
@@ -0,0 +1,3 @@
+from playsound import playsound
+playsound('Projeto_final/alarme.mp3')
+
